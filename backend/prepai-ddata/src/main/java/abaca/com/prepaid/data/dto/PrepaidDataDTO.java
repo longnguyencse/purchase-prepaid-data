3d0310c75f2b9917f0e80d9c370311b3178d809f
long.nguyen <longnguyenqu@gmail.com>
2021-04-27 17:58:38 +0700

    add core app code

--- backend/prepai-ddata/src/main/java/abaca/com/prepaid/data/dto/PrepaidDataDTO.java
+++ backend/prepai-ddata/src/main/java/abaca/com/prepaid/data/dto/PrepaidDataDTO.java
@@ -1,11 +1,11 @@
 package abaca.com.prepaid.data.dto;
 
+import java.math.BigDecimal;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
