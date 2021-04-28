ed0d933c76acadb7323f97e869d79b0e001da708
long.nguyen <long.nguyen@jiohealth.com>
2021-04-29 06:48:30 +0700

    ci/cd test push image l20

--- backend/voucher/build_docker.sh
+++ backend/voucher/build_docker.sh
@@ -13,14 +13,11 @@ fi
 # 1. build jar file
 ./gradlew bootJar
 
-# new login with new container registry url and PAT
-#echo "${CR_PAT}"
-#echo "${CR_PAT}" | docker login ghcr.io -u "$GITHUB_ACTOR" --password-stdin
-#
-## $(git rev-parse --abbrev-ref HEAD)
-#docker build \
-#  -t "${registry}"/"${project}":"$(git rev-parse --short HEAD)" \
-#  -t "${registry}"/"${project}":"${environment}"latest .
-#
-#docker push "${registry}"/"${project}":"$(git rev-parse --short HEAD)"
-#docker push "${registry}"/"${project}":"${environment}"latest
+echo "${CR_PAT}" | docker login ghcr.io -u "$GITHUB_ACTOR" --password-stdin
+
+docker build \
+  -t "${registry}"/"${project}":"$(git rev-parse --short HEAD)" \
+  -t "${registry}"/"${project}":"${environment}"latest .
+
+docker push "${registry}"/"${project}":"$(git rev-parse --short HEAD)"
+docker push "${registry}"/"${project}":"${environment}"latest
