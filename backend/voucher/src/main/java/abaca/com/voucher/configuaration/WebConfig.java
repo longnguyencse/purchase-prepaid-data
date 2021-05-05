e68446f1554121be5f65e6e4e4cbef2d6050923e
long.nguyen <long.nguyen@jiohealth.com>
2021-05-05 19:48:25 +0700

    add web request interceptor

---
+++ backend/voucher/src/main/java/abaca/com/voucher/configuaration/WebConfig.java
@@ -0,0 +1,21 @@
+package abaca.com.voucher.configuaration;
+
+import abaca.com.voucher.interceptor.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor);
+    }
+}
